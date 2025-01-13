00f0b765c59ab7195b59f8f68d8cd01a38776126
Karthikeyan M <karthykeyan.murthy@gmail.com>
2025-01-13 13:46:11 +0530

    2d array rotation by 90 degrees with a temp array

---
+++ matrixRotateby90Degrees.java
@@ -0,0 +1,48 @@
+/*
+ *  With Space Complexity of O(m x n)
+ * as new RES array is usd to store result
+ * 
+ * RES array traversal with [j][rows - i - 1]
+ */
+public class matrixRotateby90Degrees {
+    public static int[][] res;
+
+    static void rotateMatrix(int[][] matarr)
+    {
+        int rows = matarr.length;
+        int cols = matarr[0].length;
+
+        res = new int[rows][cols];
+
+        for (int i = 0; i < rows ; i++)
+        {
+            for( int j = 0; j < cols; j++)
+            {
+                res[j][rows - i -1] = matarr[i][j];
+            }
+        }
+    }
+
+    static void printmatrix(int[][] matarr)
+    {
+        for (int[] matarr1 : matarr) {
+            for (int j = 0; j < matarr1.length; j++) {
+                System.out.print(" " + matarr1[j] + ",");
+            }
+            System.out.println();
+        }
+    }
+    public static void main (String[] args)
+    {
+        int[][] mat = {
+            {1,2,3,4,5},
+            {6,7,8,9,10},
+            {11,12,13,14,15},
+            {16,17,18,19,20},
+            {21,22,23,24,25}
+        };
+
+        rotateMatrix(mat);
+        printmatrix(res);
+    }
+}
