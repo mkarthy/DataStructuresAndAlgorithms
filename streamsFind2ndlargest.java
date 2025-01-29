563219b3df01c162e2f72b4971e5d5f8349d84f6
Karthikeyan M <karthykeyan.murthy@gmail.com>
2025-01-29 23:41:10 +0530

    another option for dinding kth largest using streams

--- streamsFind2ndlargest.java
+++ streamsFind2ndlargest.java
@@ -1,10 +1,11 @@
 
 import java.util.Arrays;
+import java.util.Optional;
 
 public class streamsFind2ndlargest {
     public static void main(String[] args) {
         int[] arr = {10, 5, 7, 3, 8, 4};
-
+/* 
         int res = Arrays.stream(arr)
                     .boxed()
                     .sorted((a,b) -> b - a)
@@ -13,5 +14,15 @@ public class streamsFind2ndlargest {
                     .orElseThrow(() -> new IllegalArgumentException("Wrong Argument"));
 
         System.err.println(" Second Largest : " + res);
+*/
+
+        Optional<Integer> result = Arrays.stream(arr)
+                             .distinct()
+                             .boxed()
+                             .sorted((a,b) -> b - a)
+                             .skip(1)
+                             .findFirst();
+
+        System.out.println(result.isPresent() ? " Second Largest :" + result.get() : "Not found");
     }
 }
